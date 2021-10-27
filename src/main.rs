ebc4a7d40ec6c5eb0a3fa277b0ce01a5bfd565be
Gasu16 <rosario.matteog@gmail.com>
2021-10-27 03:23:26 +0200

    Aggiunto kick, implementare ban, mute e le macro

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,151 @@
+#![allow(unused_imports)]
+use teloxide::{prelude::*, types::{ChatPermissions, Me}, utils::command::BotCommand};
 use std::env;
-use futures::StreamExt;
-use telegram_bot::*;
+use std::error::Error;
+use std::str;
+use std::process::Command;
+//use self::chrono::prelude::*;
+//use self::chrono::{DateTime, Duration, NaiveDateTime, Utc};
 
-// Struct di esempio per i comandi [PURO SCOPO DIDATTICO!]
-
-/*
-pub struct Commands {
-    pub kick: Command<kick>,
-    pub ban:  Command<ban>,
-}*/
-
-/*
-
-   struct Commands {
-    // Some commands
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Commands {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a macro.", parse_with = "split")]
+    Macro {option: String, macro_str: String},
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+    #[command(description = "handle a command admin.")]
+    Ban(String),
+    #[command(description = "kick a user.")]
+    Kick,
 }
 
-let cmd = Commands {
-    // Implement commands
-};
 
-*/
+type Cx = UpdateWithCx<AutoSend<Bot>, Message>;
 
-pub struct ChatMember {
-    pub user: Box<User>,
-    pub status: ChatMemberStatus,
-//  pub until_date: Option<Time>,
-    pub can_be_edited: Option<bool>,
-    pub can_change_info: Option<bool>,
-    pub can_post_messages: Option<bool>,
-    pub can_edit_messages: Option<bool>,
-    pub can_delete_messages: Option<bool>,
-    pub can_invite_users: Option<bool>,
-    pub can_restrict_members: Option<bool>,
-    pub can_pin_messages: Option<bool>,
-    pub can_promote_members: Option<bool>,
-    pub can_send_messages: Option<bool>,
-    pub can_send_media_messages: Option<bool>,
-    pub can_send_other_messages: Option<bool>,
-    pub can_add_web_page_previews: Option<bool>,
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-    let api = Api::new(token);
-
-    // Fetch new updates via long poll method
-    let mut stream = api.stream();
-    while let Some(update) = stream.next().await {
-        // If the received update contains a new message...
-        let update = update?;
-
-        if let UpdateKind::Message(message) = update.kind {
-
-            if let MessageKind::Text { ref data, .. } = message.kind {
-
-                println!("{}", &message.from.id);
-                println!("<{}>: {}", &message.from.first_name, data);
-                match data.as_str() {
-
-                    "-h" | "--help" => {
-                        api.send(message.text_reply(format!(
-                            "Lista Comandi\n ban\n kick", 
-                        )))
-                        .await?;
-                    },
-
-                    "-q" | "--quit" => {
-                        api.send(message.text_reply(format!(
-                            "Thread chiuso",
-                        )))
-                        .await?;
-                    },
-
-                    "/ban"          => {
-
-                        api.send(message.text_reply(format!(
-                            "Sei stato bannato {}",&message.from.first_name,
-                        )))
-                        .await?;
-                    },
-
-                    "/kick"         => {
-                        api.send(message.text_reply(format!(
-                            "Sei stato kickato {}",&message.from.first_name,
-                        )))
-                        .await?;
-                    },
-
-                    "/warn"         => {
-                        api.send(message.text_reply(format!(
-                            "Sei stato warnato {}",&message.from.first_name,
-                        )))
-                        .await?;
-                    },
-
-                    "/mute"         => {
-                        api.send(message.text_reply(format!(
-                            "Sei stato mutato {}",&message.from.first_name,
-                        )))
-                        .await?;
-                    },
-
-                    ".."            => {
-                        api.send(message.text_reply(format!(
-                            "...",
-                        )))
-                        .await?;
-                    },
-
-                    "-c" | "--command" => {
-                        // Interpreta i comandi bash, come echo per esempio
-                    },
-
-                    _               => println!("Questo comando non esiste"),
-                    
-                }
-            }
+/*
+// Muta un utente rispondendo a un suo messaggio
+async fn mute_user(cx: &Cx, time: Duration) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match cx.update.reply_to_message() {
+        Some(msg1) => {
+            cx.requester
+                .restrict_chat_member(
+                    cx.update.chat_id(),
+                    msg1.from().expect("Must be MessageKind::Common").id,
+                    ChatPermissions::default(),
+                )
+                .until_date(
+                    DateTime::<Utc>::from_utc(
+                        NaiveDateTime::from_timestamp(cx.update.date as i64, 0),
+                        Utc,
+                    ) + time,
+                )
+                .await?;
+        }
+        None => {
+            cx.reply_to("Devi usare questo comando in risposta a un messaggio").send().await?;
         }
     }
     Ok(())
+}
+*/
+
+// Kicka un utente
+async fn kick_user(cx: &Cx) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match cx.update.reply_to_message() {
+        Some(mes) => {
+            // bot.unban_chat_member can also kicks a user from a group chat.
+            cx.requester
+                .unban_chat_member(cx.update.chat_id(), mes.from().unwrap().id)
+                .send()
+                .await?;
+        }
+        None => {
+            cx.reply_to("Usa questo comando in risposta ad un messaggio").send().await?;
+        }
+    }
+    Ok(())
+}
+
+async fn answer(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    command: Commands,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+
+        Commands::Help => cx.answer(Commands::descriptions()).await?,
+
+        Commands::Username(username)             => {
+            cx.answer(format!("Your username is @{}.", username)).await?
+        }
+
+        Commands::UsernameAndAge{username, age}  => {
+            cx.answer(format!("Your username is @{} and age is {}.", username, age)).await?
+        }
+
+        Commands::Kick                           => {
+            kick_user(&cx).await?;
+            cx.answer(format!("Utente Kickato")).await?
+        }
+
+        Commands::Macro{option, macro_str}       => {
+            
+            match option.as_str() {
+            
+                "-a" | "--add"                  => {
+                    cx.answer(format!("Macro {} aggiunta con opzione {}", macro_str, option)).await?
+                },
+
+                "-e" | "--edit"                 => {
+                    cx.answer(format!("Macro editata")).await?
+                },
+
+                "-r" | "--remove"               => {
+                    cx.answer(format!("Macro rimossa")).await?
+                }
+
+                "-c" | "--to-ascii"              => {
+                    
+                    let mut cmd = Command::new("sh");
+                    let j = ["echo", macro_str.as_str()].join(" ");
+                    cmd.arg("-c").arg(j);
+                    let _cmd = cmd.output().expect("Comando non letto correttamente");
+                         
+                    cx.answer(format!("{:?}", _cmd.stdout)).await?
+
+                }
+
+                _                               =>  cx.answer("Comando non valido").await? // Wildcard
+            
+            }
+        }
+
+        Commands::Ban(stringa)                   => {
+            cx.answer(format!("Sei stato bannato {}",stringa)).await?
+        }
+    };
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn run() {
+    teloxide::enable_logging!();
+    log::info!("Starting simple_commands_bot...");
+
+    let _bot = Bot::from_env().auto_send();
+
+    let Me {user: _bot_user, ..} = _bot.get_me().await.unwrap();
+//    let _bot_name: String = panic!("hacktoberBot");
+    let _bot_name: String = "hacktoberBot".into();
+    teloxide::commands_repl(_bot, _bot_name, answer).await;
 }
